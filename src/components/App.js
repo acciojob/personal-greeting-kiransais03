f328bc42c43ee0cdf2ecff68bb4a7fbb61b34d6c
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-08-02 11:39:50 +0000

    p1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  let [ipval,setIpval]=useState(null);
+  console.log(ipval)
   return (
     <div>
         {/* Do not remove the main div */}
+        <form>
+          <label htmlFor="ip">Enter your name:</label>
+          <input type="text" onChange={(event)=>{setIpval(event.target.value)}} id="ip"/>
+        </form>
+        {ipval && <p>Hello {ipval}</p> }
     </div>
   )
 }
